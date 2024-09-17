f44bed08630f65cf537e5ac4aaa5e87ae90d3590
Ngumonelson123 <nelsonmbui88@gmail.com>
2024-09-17 10:51:38 +0000

    main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+<% if (user) { %>
+    <h2><%= user.name %></h2>
+  <% } %>
+  
+  let template = ejs.compile(str, options);
+  template(data);
+  // => Rendered HTML string
+  
+  ejs.render(str, data, options);
+  // => Rendered HTML string
+  
+  ejs.renderFile(filename, data, options, function(err, str){
+      // str => Rendered HTML string
+  });
